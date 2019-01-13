a3b8b668bf66e2ed62cd5ab64993377bb59e4d84
Melissa Real <melissa.real@hotmail.com>
2019-01-13 20:57:52 +0000

    Automatically backed up by Learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,13 @@ class Note < ActiveRecord::Base
   # add associations here
   validates_presence_of :content
   belongs_to :song
+
+
+  def song_ids=(ids)
+    ids.each do |id|
+      song = Song.find(id)
+      self.notes << note
+    end
+  end
+
 end
